f3f125ee133673561da270c5506047dd3955a967
mengyuan21 <15332392019@163.com>
2022-11-03 20:16:37 +0800

    [Mia] feat: reset the app.js with specific block

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,7 +2,38 @@ import './App.css';
 
 function App() {
   return (
-    <div>Hello world </div>
+    <div className='categories-container'> 
+      <div className='category-container'>
+        <div className='category-body-container'>
+          <h2> Hats </h2>
+          <p> shop now </p>
+        </div>
+      </div>
+      <div className='category-container'>
+        <div className='category-body-container'>
+          <h2> Jackets </h2>
+          <p> shop now </p>
+        </div>
+      </div>
+      <div className='category-container'>
+        <div className='category-body-container'>
+          <h2> Sneakers </h2>
+          <p> shop now </p>
+        </div>
+      </div>
+      <div className='category-container'>
+        <div className='category-body-container'>
+          <h2> Womens </h2>
+          <p> shop now </p>
+        </div>
+      </div>
+      <div className='category-container'>
+        <div className='category-body-container'>
+          <h2> Mens </h2>
+          <p> shop now </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
